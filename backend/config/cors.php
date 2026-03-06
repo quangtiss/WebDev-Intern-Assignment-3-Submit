700a34d5e48ea605723b929ede6c76a1384e790d
quangtiss <quangpro0166@gmail.com>
2026-03-06 14:53:01 +0700

    config CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://web-dev-intern-assignment-3-submit.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
