9fd736ee6332794163eaaf774f97690ae52b04e7
quangtiss <quangpro0166@gmail.com>
2026-03-06 08:48:57 +0700

    add api of three feature

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScoreController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('api')->group(
+    function () {
+        #Feature 1
+        Route::get('/score/{sbd}', [ScoreController::class, 'findBySbd']);
+
+        #Feature 2
+        Route::get('/stats', [ScoreController::class, 'getStats']);
+
+        #Feature 3
+        Route::get('/top10-group-a', [ScoreController::class, 'getTop10GroupA']);
+    }
+);
